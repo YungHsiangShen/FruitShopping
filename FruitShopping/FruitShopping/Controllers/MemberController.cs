3fc9ec923647f7808e6beb3f82fd94f77f4bf49e
krn0829 <krn0829@gmail.com>
2023-08-20 13:57:35 +0800

    上傳水果照片和會員中心測式頁面

---
+++ FruitShopping/FruitShopping/Controllers/MemberController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FruitShopping.Controllers
+{
+	public class MemberController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult customer_service()
+		{
+			return View();
+		}
+
+		public IActionResult favorite_list()
+		{
+			return View();
+		}
+
+		public IActionResult order_history()
+		{
+			return View();
+		}
+
+    }
+}

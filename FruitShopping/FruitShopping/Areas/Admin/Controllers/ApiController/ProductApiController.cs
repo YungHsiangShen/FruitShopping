a3065e93c2546c6d497e3216959059379c6ba80d
Sunny <love1780434379@gmail.com>
2023-08-22 04:18:35 +0800

    UpDate ProductManager

--- FruitShopping/FruitShopping/Areas/Admin/Controllers/ApiController/ProductApiController.cs
+++ FruitShopping/FruitShopping/Areas/Admin/Controllers/ApiController/ProductApiController.cs
@@ -1,16 +1,13 @@
 ﻿using FruitShopping.Areas.Admin.Models.ViewModel;
 using FruitShopping.Models;
 using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.ComponentModel;
-using System.Linq;
 
 namespace FruitShopping.Areas.Admin.Controllers.ApiController
 {
 	[EnableCors("AllowAny")]
-	[Route("api/ProductApi")]
+	[Route("api/ProductApi/{action}")]
 	[ApiController]
 	public class ProductApiController : ControllerBase
 	{
@@ -26,8 +23,34 @@ namespace FruitShopping.Areas.Admin.Controllers.ApiController
 		public async Task<IEnumerable<ProductsManagershowDTO>> GetAllProducts()
 		{
 			return _db.Products
-			.Include(c => c.Category)
+			.Include(p => p.Category)
 			.Include(p => p.Supplier)
+			.Select(p => new ProductsManagershowDTO
+			{
+				ProductId = p.ProductId,
+				ProductName = p.ProductName,
+				CostPrice = p.CostPrice,
+				UnitPrice = p.UnitPrice,
+				CategoryName = p.Category.CategoryName,
+				SupplierName = p.Supplier.ContactName,
+				PlaceOfOriginName = p.PlaceOfOrigin.Name
+			});
+		}
+		//api/ProductApi("Filter")
+		[HttpPost("Filter")]
+		public async Task<IEnumerable<ProductsManagershowDTO>> FilterProduct(ProductsManagershowDTO pmDTO)
+		{
+			 return _db.Products
+			.Include(p => p.Category)
+			.Include(p => p.Supplier)
+			.Where(p =>
+				p.ProductId == pmDTO.ProductId ||
+				p.ProductName.Contains(pmDTO.ProductName) ||
+				(p.CostPrice >= pmDTO.CostPrice) ||
+				(p.UnitPrice >= pmDTO.UnitPrice) ||
+				p.Category.CategoryName.Contains(pmDTO.CategoryName) ||
+				p.Supplier.ContactName.Contains(pmDTO.SupplierName) ||
+				p.PlaceOfOrigin.Name.Contains(pmDTO.PlaceOfOriginName))
 			.Select(p => new ProductsManagershowDTO
 			{
 				ProductId = p.ProductId,
